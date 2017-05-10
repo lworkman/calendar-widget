817c66a50bc34fee8a7d5160b1ace1de24deaf63
lworkman <liam.workman@gmail.com>
2017-05-09 20:39:22 -0700

    first working version

--- client/src/containers/calendar-widget.js
+++ client/src/containers/calendar-widget.js
@@ -1,25 +1,42 @@
 import React from "react";
+import CalendarCard from "../components/calendar-card.js";
 
 export default class CalendarWidget extends React.Component{
 
     constructor(){
         super();
         this.state = {
-            'dataReceived': false
+            'dataReceived': false,
+            'data': []
         };
+        this.sendGETRequest('./api');
     }
 
-    sendGETRequest(){
+    sendGETRequest(url){
         let requestContainer = new XMLHttpRequest();
-        requestContainer.open('GET','./api',true);
-        requestContainer.onreadystatechange = function(){
-            console.log(JSON.parse(this.responseText));
-        };
+        requestContainer.open('GET',url,true);
+        requestContainer.onreadystatechange = this.processGETRequest.bind(this,requestContainer);
         requestContainer.send();
     }
 
+    processGETRequest(response){
+        if (response.readyState === 4 && response.status === 200){
+            this.setState({
+                'dataReceived': true,
+                'data': JSON.parse(response.responseText)
+            });
+        }
+    }
+
     render(){
-        this.sendGETRequest();
-        return <p>Hello</p>
+        let elements = [];
+        if (this.state.dataReceived){
+            let properData = this.state.data.events;
+
+            for (let i = 0; i < properData.length; i++){
+                elements.push(<CalendarCard key={i} info={properData[i]} />);
+            };
+        }
+        return <div>{elements}</div>
     }
 }
